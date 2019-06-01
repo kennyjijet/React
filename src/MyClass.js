970cbfb46452b31295eb272f34f5132b77f55768
kennyjijet <usistal@hotmail.com>
2019-06-01 14:47:18 +1200

    Initial_Git
    Initial_Git

--- src/MyClass.js
+++ src/MyClass.js
@@ -1,8 +1,15 @@
-let names = ['Jake', 'Jon', 'Thruster'];
-var peopleToReturn = [];
-const peopleList = () => {
-  for (let i = 0; i < names.length; i++) {
-    peopleToReturn.push(<li>{names[i]}</li>);
+import React, { Component } from "react";
+class MyClass extends Component {
+  render() {
+    let names = ["Jake", "Jon", "Thruster"];
+    var peopleToReturn = [];
+    const peopleList = () => {
+      for (let i = 0; i < names.length; i++) {
+        peopleToReturn.push(<li>{names[i]}</li>);
+      }
+      return peopleToReturn;
+    };
+    return <ol>{peopleList()}</ol>;
   }
-  return peopleToReturn;
-};
+}
+export default MyClass;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import MyClass from './MyClass';
-
+import MyClass from "./MyClass.js";
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <h2>Start editing to see some magic happen!</h2>
-      
-      <div className="List">
-        <ol>{MyClass.peopleList()}</ol>
-      </div>
+      <MyClass />
     </div>
-
   );
 }
-
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
