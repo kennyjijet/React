ab744823d0483c24b2e8b54b9351594292be7ca4
kennyjijet <usistal@hotmail.com>
2019-06-12 16:17:04 +1200

    link

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
-import MyClass from "./MyClass.js";
-import { Router, hashHistory as history } from "react-router";
-import routes from "./routes";
+import App from "./index2";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h2>Start editing to see some magic happen!</h2>
-      <MyClass />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
